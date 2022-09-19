f537ef4c572b2b356e919cdfc4985aef34e2f509
a-hryhorenko <113055646+a-hryhorenko@users.noreply.github.com>
2022-09-19 15:09:03 +0300

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                {
+                    Console.Write("Радiус: ");
+                    double R = Convert.ToDouble(Console.ReadLine());
+                    if (R > 0) {
+                        double S = 4 * Math.PI * (R * R);
+                        double V = 4 * Math.PI * (R * R * R) / 3;
+                        Console.WriteLine("Площа кулi: {0} Об'єм кулi: {1}", S, V);
+                        Console.ReadKey();
+                        return;             
+                    }
+                    else
+                        Console.WriteLine("Введiть вiрне: ");
+                }
+            }
+        }
+    }
+}
+
+
