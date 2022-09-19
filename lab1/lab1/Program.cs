d1f67c54d243cc652456cb988c71d7fd56895645
a-hryhorenko <113055646+a-hryhorenko@users.noreply.github.com>
2022-09-19 16:55:17 +0300

    Update Program.cs

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -6,20 +6,19 @@ namespace lab1
     {
         static void Main()
         {
-            while (true)
+            double R = 0;
+            bool isROk = false;
+            do
             {
-                {
-                    Console.Write("Радiус: ");
-                    double R = Convert.ToDouble(Console.ReadLine());
-                    if (R > 0) {
-                        double S = 4 * Math.PI * (R * R);
-                        double V = 4 * Math.PI * (R * R * R) / 3;
-                        Console.WriteLine("Площа кулi: {0} Об'єм кулi: {1}", S, V);
-                        Console.ReadKey();
-                        return;             
-                    }
-                    else
-                        Console.WriteLine("Введiть вiрне: ");
+                Console.Write("Радiус: ");
+                string strR = Console.ReadLine();
+                isROk = double.TryParse(strR, out R);
+                if (!isROk)
+                Console.WriteLine("Помилка введення напруги");
+            } while (!isROk);
+            double S = 4 * Math.PI * (R * R), V = 4 * Math.PI * (R * R * R) / 3;
+            Console.WriteLine("Площа кулi: {0} Об'єм кулi: {1}", S, V);
+            Console.ReadKey();
                 }
             }
         }
